e6a4f668cacab4a0cef2a1142067ba545fc5ddb9
Matheus Cruz Martins <117648175+matheuscmartins@users.noreply.github.com>
2023-01-08 10:17:21 -0300

    Seller TableView

--- src/main/java/com/udemy/projectjavafxjdbc/gui/SellerListController.java
+++ src/main/java/com/udemy/projectjavafxjdbc/gui/SellerListController.java
@@ -25,6 +25,7 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 import java.util.ResourceBundle;
@@ -40,6 +41,12 @@ public class SellerListController implements Initializable, DataChangeListener {
 
     @FXML
     private TableColumn<Seller, String> tableColumnName;
+    @FXML
+    private TableColumn<Seller, String> tableColumnEmail;
+    @FXML
+    private TableColumn<Seller, Date> tableColumnBirthDate;
+    @FXML
+    private TableColumn<Seller, Double> tableColumnBaseSalary;
     @FXML
     private TableColumn<Seller, Seller> tableColumnEDIT;
 
@@ -71,6 +78,11 @@ public class SellerListController implements Initializable, DataChangeListener {
         //padrão para inicializar o padrão das colunas
         tableColumnId.setCellValueFactory(new PropertyValueFactory<>("id"));
         tableColumnName.setCellValueFactory(new PropertyValueFactory<>("name"));
+        tableColumnEmail.setCellValueFactory(new PropertyValueFactory<>("email"));
+        tableColumnBirthDate.setCellValueFactory(new PropertyValueFactory<>("birthDate"));
+        Utils.formatTableColumnDate(tableColumnBirthDate, "dd/MM/yyyy");
+        tableColumnBaseSalary.setCellValueFactory(new PropertyValueFactory<>("baseSalary"));
+        Utils.formatTableColumnDouble(tableColumnBaseSalary, 2);
 
         Stage stage = (Stage) Main.getMainScene().getWindow(); //faz um downcasting para Stege
         sellerTableView.prefHeightProperty().bind(stage.heightProperty()); //faz o TableView acompanhar a altura da janela
